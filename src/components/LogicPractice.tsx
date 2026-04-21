241a233145bf3a661341ac73cee8f45dfb7e80e3
mmm-maymyatmon <maymyat.dev@gmail.com>
2026-04-21 16:33:14 +0700

    data fetching

--- src/components/LogicPractice.tsx
+++ src/components/LogicPractice.tsx
@@ -1,25 +1,68 @@
-type Product = {
-    id: number;
-    name: string;
-    price: number;
-    stock: number;
-}
-interface LogicPracticeProps {
-    products: Product[]
-}
+import React, { useEffect, useMemo, useState } from "react";
 
-function LogicPractice({ products }: LogicPracticeProps) {
-    
-    const filterAndSortProducts = products.filter((product)=>product.stock > 0).sort((a,b)=> a.price - b.price)
+type LogicPracticeProp = {
+  id: number;
+  title: string;
+  price: number;
+  description?: string;
+  category: string;
+  image?: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+function LogicPractice() {
+  const [products, setProducts] = useState<LogicPracticeProp[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `${import.meta.env.VITE_API_BASE_URL}/products`,
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (err: unknown) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const filterAndSortProduct = useMemo(() => {
+    return products
+      .filter((product) => product.rating?.count > 100)
+      .sort((a, b) => a.price - b.price);
+  }, [products]);
+
+  if (loading) return <p>Loading....</p>;
+  if (error) return <p>Error: {error}</p>;
   return (
-      <div>
-          {
-              filterAndSortProducts.map((product) => (
-                  <p key={product.id}>{product.name}</p>
-              ))
-          }
+    <div>
+      {filterAndSortProduct.length === 0 ? (
+        <p>No products found</p>
+      ) : (
+        filterAndSortProduct.map((p) => (
+          <div key={p.id}>
+            <p>{p.title}</p>
+            <p>{p.price}</p>
+            <p>{p.rating.count}</p>
+          </div>
+        ))
+      )}
     </div>
-  )
+  );
 }
 
-export default LogicPractice
+export default LogicPractice;
