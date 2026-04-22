9f0819c01a2fbd85af0c522331cd836ec5fe40cb
mmm-maymyatmon <maymyat.dev@gmail.com>
2026-04-22 08:30:01 +0700

    Initial commit for Day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import LogicPractice from './components/LogicPractice'
 
 function App() {
--- src/components/LogicPractice.tsx
+++ src/components/LogicPractice.tsx
@@ -1,68 +1,66 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from 'react'
 
-type LogicPracticeProp = {
-  id: number;
-  title: string;
-  price: number;
-  description?: string;
-  category: string;
-  image?: string;
-  rating: {
-    rate: number;
-    count: number;
-  };
-};
-
-function LogicPractice() {
-  const [products, setProducts] = useState<LogicPracticeProp[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(
-          `${import.meta.env.VITE_API_BASE_URL}/products`,
-        );
-
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setProducts(data);
-      } catch (err: unknown) {
-        setError((err as Error).message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  const filterAndSortProduct = useMemo(() => {
-    return products
-      .filter((product) => product.rating?.count > 100)
-      .sort((a, b) => a.price - b.price);
-  }, [products]);
-
-  if (loading) return <p>Loading....</p>;
-  if (error) return <p>Error: {error}</p>;
-  return (
-    <div>
-      {filterAndSortProduct.length === 0 ? (
-        <p>No products found</p>
-      ) : (
-        filterAndSortProduct.map((p) => (
-          <div key={p.id}>
-            <p>{p.title}</p>
-            <p>{p.price}</p>
-            <p>{p.rating.count}</p>
-          </div>
-        ))
-      )}
-    </div>
-  );
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    images: string;
+    rating: {
+        rate: number;
+        count: number;
+    }
 }
 
-export default LogicPractice;
+function LogicPractice() {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/products`)
+
+                if (!response.ok) {
+                    throw new Error('Network Error')
+                }
+                const data = await response.json();
+                setProducts(data)
+            } catch (err: unknown) {
+                setError((err as Error).message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData();
+    },[])
+
+    const filterAndSortProduct = products.filter((product)=>product.rating?.count > 200).sort((a,b)=> a.price - b.price)
+
+    if (loading) {
+        return <p>Loading</p>
+    }
+    if (error) {
+        return <p>Error: {error}</p>
+    }
+  return (
+      <div>
+          {
+              filterAndSortProduct.length === 0 ? (
+                  <p>No product found</p>
+              ) : (
+                      filterAndSortProduct.map((product) => (
+                          <div key={product.id} >
+                              <p>{product.title}</p>
+                          <p>{product.price}</p>
+                          </div>
+                      ))
+              )
+          }
+    </div>
+  )
+}
+
+export default LogicPractice
